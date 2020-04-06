ddc4cc504db99488628309f1b20e2bf07ad5f285
ebaymademepoor <caianroma@yahoo.co.uk>
2020-04-06 09:43:18 +0100

    Updated heroku allowed host in settings

--- myTeam/settings.py
+++ myTeam/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = env.str('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", "https://my-team-utility-2020.herokuapp.com/"]
+ALLOWED_HOSTS = ["127.0.0.1", "my-team-utility-2020.herokuapp.com"]
 
 # Application definition
 
